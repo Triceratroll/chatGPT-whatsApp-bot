a8eef5e05a8160a5baa866a4ef561cf17b3dc91c
Triceratroll <adrianmf06@gmail.com>
2022-12-23 14:17:34 +0100

    mod working example

--- example.py
+++ example.py
@@ -1,59 +1,24 @@
 import os
 import openai
-import speech_recognition as sr
 from dotenv import load_dotenv
-from gtts import gTTS
-from playsound import playsound 
 
-def jarvis_speaks(text):
-
-  print(f'Jarvis: {text}') 
-  tts = gTTS(text, lang='es', tld='es')
-
-  if os.path.exists("./audio.mp3"):
-    os.remove("./audio.mp3")
-
-  tts.save('./audio.mp3') 
-  playsound('./audio.mp3')
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def main():
+def main(): 
 
-  load_dotenv()
-  openai.api_key = os.getenv("OPENAI_API_KEY") 
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
+    temperature=0.3,
+    max_tokens=100,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
 
-  # Create a Recognizer instance
-  r = sr.Recognizer()
-  # Create a microphone instance
-  mic = sr.Microphone()
-
-  jarvis_speaks('Hola soy Jarvis a su servicio')
-
-  while True:
-
-    with mic as source:
-
-      r.adjust_for_ambient_noise(source)
-      audio = r.listen(source) 
-
-    try:
-
-      text = r.recognize_google(audio, language='es-ES')
-      print(f'You said: {text}')
-
-      response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=text,
-        temperature=0.6,
-        max_tokens=250, 
-      )
-      response_text = response.choices[0].text
-      jarvis_speaks(response_text)
-
-    except sr.UnknownValueError:
-      print('Sorry, I could not understand what you said')
-    except sr.RequestError as e:
-      print(f'Error while making the request: {e}')
+  print(response.choices[0].text)
       
 if __name__ == "__main__":
   main()
